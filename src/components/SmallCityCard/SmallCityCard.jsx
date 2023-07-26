779491a46f0ca66908962a2df0166adf27366da5
NiklasRulec <niklasrulec.98@gmail.com>
2023-07-26 16:00:13 +0200

    upload

--- src/components/SmallCityCard/SmallCityCard.jsx
+++ src/components/SmallCityCard/SmallCityCard.jsx
@@ -3,7 +3,7 @@ import "./SmallCityCard.css";
 const SmallCityCard = (props) => {
   return (
     <article className="small-city-card">
-      <h2>{props.name}</h2>
+      <h3>{props.name}</h3>
       <h3>{props.temp}</h3>
     </article>
   );
